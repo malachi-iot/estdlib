82c617ba45be5b4f6b817d1402b96b6140af3405
Malachi Burke <malachi.burke@gmail.com>
2018-04-30 10:15:58 -0700

    Moving in some platform.h stuff from mem lib

--- src/platform.h
+++ src/platform.h
@@ -1,21 +1,33 @@
 #pragma once
 
 #if __cplusplus >= 201103L
-#define FEATURE_CPP_MOVESEMANTIC
 #define FEATURE_CPP_ALIASTEMPLATE
+#define FEATURE_CPP_CONSTEXPR
+#define FEATURE_CPP_DECLTYPE
+#define FEATURE_CPP_DECLVAL
+#define FEATURE_CPP_MOVESEMANTIC
 #define FEATURE_CPP_STATIC_ASSERT
 #define FEATURE_CPP_VARIADIC
-#define FEATURE_CPP_CONSTEXPR
 #define FEATURE_CPP_LAMBDA
 #define FEATURE_CPP_RANGED_FORLOOP
 #define FEATURE_CPP_INITIALIZER_LIST
 #define FEATURE_CPP_DEFAULT_FUNCDEF
 
+#if defined(__STDC_LIB_EXT1__)
+// see http://en.cppreference.com/w/c/string/byte/strncpy
+#define FEATURE_CPP_STRNCPY_S
+#endif
+
+#define OVERRIDE override
 #define CONSTEXPR constexpr
 #define NULLPTR nullptr
+
+// C++17 onward features go here
+#if __cplusplus >= 201703L
+#endif
+
 #else
+#define OVERRIDE
 #define CONSTEXPR const
 #define NULLPTR NULL
 #endif
-
-//#include_next "platform.h"
