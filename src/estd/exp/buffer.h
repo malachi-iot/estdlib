2d8464bfd5375fdf402c9bc798c1d3d8e5281cbe
Malachi Burke <malachi.burke@gmail.com>
2018-06-30 10:28:01 -0700

    Switching 'layer3::buffer' to 'span'

--- src/estd/exp/buffer.h
+++ src/estd/exp/buffer.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "../array.h"
+#include "../span.h"
 #include "../type_traits.h"
 
 namespace estd {
@@ -60,34 +61,9 @@ public:
 
 namespace layer3 {
 
-// move & rename this to span , but keep the const_buffer since it's so darned convenient
-template <class T, class TSize = size_t>
-class buffer : public estd::layer3::array<T, size_t>
-{
-    typedef estd::layer3::array<T, size_t> base_t;
-    typedef typename base_t::size_type size_type;
-    typedef typename base_t::value_type value_type;
 
-public:
-    // This is a low level call, but buffers are low level creatures
-    // gently discouraged during mutable_buffer,
-    // strongly discouraged during const_buffer,
-    // but not necessarily wrong to use it
-    void resize(size_type n) { base_t::m_size = n; }
-
-    buffer(value_type* data, size_type size) :
-        base_t(data, size) {}
-
-    template <size_t N>
-    buffer(value_type (&data) [N]) : base_t(data, N) {}
-
-    // most definitely a 'shallow clone'
-    buffer(const buffer& clone_from) :
-        base_t(clone_from.data(), clone_from.size()) {}
-};
-
-typedef buffer<uint8_t> mutable_buffer;
-typedef buffer<const uint8_t> const_buffer;
+typedef span<uint8_t> mutable_buffer;
+typedef span<const uint8_t> const_buffer;
 
 
 }
--- src/estd/span.h
+++ src/estd/span.h
@@ -1,6 +1,33 @@
 #pragma once
 
-// TODO: Yank in layer3::buffer here
+#include "array.h"
+
 namespace estd {
+
+// TODO: once we lean array itself on allocated_array, beef up constructors here
+template <class T, class TSize = size_t>
+class span : public estd::layer3::array<T, size_t>
+{
+    typedef estd::layer3::array<T, size_t> base_t;
+    typedef typename base_t::size_type size_type;
+    typedef typename base_t::value_type value_type;
+
+public:
+    // This is a low level call, but buffers are low level creatures
+    // gently discouraged during mutable_buffer,
+    // strongly discouraged during const_buffer,
+    // but not necessarily wrong to use it
+    void resize(size_type n) { base_t::m_size = n; }
+
+    span(value_type* data, size_type size) :
+            base_t(data, size) {}
+
+    template <size_t N>
+    span(value_type (&data) [N]) : base_t(data, N) {}
+
+    // most definitely a 'shallow clone'
+    span(const span& clone_from) :
+            base_t(clone_from.data(), clone_from.size()) {}
+};
 
 }
