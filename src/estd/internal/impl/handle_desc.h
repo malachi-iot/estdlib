b6fb73cd256eb063fe29028c02e139126086d5d9
Malachi Burke <malachi.burke@gmail.com>
2018-07-20 13:46:29 -0700

    Code cleanup

--- src/estd/internal/impl/handle_desc.h
+++ src/estd/internal/impl/handle_desc.h
@@ -1,43 +1,11 @@
 #pragma once
 
 #include "../../traits/allocator_traits.h"
+#include "../value_evaporator.h"
 
 namespace estd { namespace internal { namespace impl {
 
-template <class TValue, bool is_present, class TEvaporated = TValue, TEvaporated default_value = TEvaporated()>
-class value_evaporator;
 
-template <class TValue, class TEvaporated, TEvaporated default_value>
-class value_evaporator<TValue, true, TEvaporated, default_value>
-{
-protected:
-    void value(TEvaporated) {}
-
-public:
-    TEvaporated value() const { return default_value; }
-
-    static CONSTEXPR bool is_evaporated = true;
-
-    value_evaporator(TEvaporated) {}
-    value_evaporator() {}
-};
-
-template <class TValue, class TEvaporated, TEvaporated default_value>
-class value_evaporator<TValue, false, TEvaporated, default_value>
-{
-protected:
-    TValue m_value;
-
-    void value(const TValue& v) { m_value = v; }
-
-public:
-    TValue value() const { return m_value; }
-
-    static CONSTEXPR bool is_evaporated = false;
-
-    value_evaporator(const TValue& v) : m_value(v) {}
-    value_evaporator() {}
-};
 
 
 // contains no members, just specialized copy helpers
@@ -142,8 +110,8 @@ protected:
 };
 
 
-template <class TAllocator, bool is_singular, class TTraits =
-        estd::allocator_traits<typename remove_reference<TAllocator>::type> >
+template <class TAllocator, bool is_singular,
+        class TTraits = estd::allocator_traits<typename remove_reference<TAllocator>::type> >
 class handle_descriptor :
         value_evaporator<typename TTraits::handle_type, is_singular, bool, true>
 {
--- src/estd/internal/value_evaporator.h
+++ src/estd/internal/value_evaporator.h
@@ -1,1 +1,42 @@
 #pragma once
+
+#include "platform.h"
+
+namespace estd { namespace internal {
+
+template <class TValue, bool is_present, class TEvaporated = TValue, TEvaporated default_value = TEvaporated()>
+class value_evaporator;
+
+template <class TValue, class TEvaporated, TEvaporated default_value>
+class value_evaporator<TValue, true, TEvaporated, default_value>
+{
+protected:
+    void value(TEvaporated) {}
+
+public:
+    TEvaporated value() const { return default_value; }
+
+    static CONSTEXPR bool is_evaporated = true;
+
+    value_evaporator(TEvaporated) {}
+    value_evaporator() {}
+};
+
+template <class TValue, class TEvaporated, TEvaporated default_value>
+class value_evaporator<TValue, false, TEvaporated, default_value>
+{
+protected:
+    TValue m_value;
+
+    void value(const TValue& v) { m_value = v; }
+
+public:
+    TValue value() const { return m_value; }
+
+    static CONSTEXPR bool is_evaporated = false;
+
+    value_evaporator(const TValue& v) : m_value(v) {}
+    value_evaporator() {}
+};
+
+}}
