e3350786cde84b82af0b9ac51bd170e96333a870
Malachi Burke <malachi.burke@gmail.com>
2018-08-12 12:36:49 -0700

    Organizing code

---
+++ src/estd/port/streambuf.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#ifdef ESP_OPEN_RTOS
+#elif defined(__MBED__)
+#include <drivers/Stream.h>
+// looks like a collision
+// on the F() macro, so undef it first
+#ifdef F
+#define F_DEFINED
+#undef F
+#endif
+#include <drivers/Serial.h>
+// redefine F here.  Kludgey for sure.  There will be situations where
+// F wants to be what Serial.h set it to
+#ifdef F_DEFINED
+//#include "noduino_F.h"
+#undef F_DEFINED
+#endif
+
+#elif defined(ARDUINO)
+#include <Arduino.h>
+#endif
