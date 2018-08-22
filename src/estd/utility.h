5e40bd8e311af3182c82970f7cef4bfacfd48434
Malachi Burke <malachi.burke@gmail.com>
2018-08-21 20:04:57 -0700

    Untested integer_sequence code

--- src/estd/utility.h
+++ src/estd/utility.h
@@ -22,6 +22,32 @@ template<
 template<class T>
 typename estd::add_rvalue_reference<T>::type declval() */
 
+#ifdef FEATURE_CPP_VARIADIC
+// adapted from https://gist.github.com/ntessore/dc17769676fb3c6daa1f
+template<typename T, T... Ints>
+struct integer_sequence
+{
+    typedef T value_type;
+    static constexpr std::size_t size() { return sizeof...(Ints); }
+};
+
+template<std::size_t... Ints>
+using index_sequence = integer_sequence<std::size_t, Ints...>;
+
+template<typename T, std::size_t N, T... Is>
+struct make_integer_sequence : make_integer_sequence<T, N-1, N-1, Is...> {};
+
+template<typename T, T... Is>
+struct make_integer_sequence<T, 0, Is...> : integer_sequence<T, Is...> {};
+
+template<std::size_t N>
+using make_index_sequence = make_integer_sequence<std::size_t, N>;
+
+template<typename... T>
+using index_sequence_for = make_index_sequence<sizeof...(T)>;
+
+#endif
+
 }
 
 #if defined(FEATURE_STD_UTILITY)
