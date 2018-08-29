44026c9f3627125c029edade496a2a1dd1dce1ce
Malachi Burke <malachi.burke@gmail.com>
2018-08-29 02:04:55 -0700

    Adding experimental freertos thread class wrapper
    Far from working

--- src/estd/port/freertos/thread.h
+++ src/estd/port/freertos/thread.h
@@ -1,8 +1,72 @@
 #pragma once
 
 #include "chrono.h"
+#include "../../functional.h"
 
-namespace estd { namespace this_thread {
+#ifdef ESP32
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#else
+#include <task.h>
+#endif
+
+
+namespace estd {
+
+class thread
+{
+    TaskHandle_t th;
+
+    template <class Function, class ... Args>
+    static void start_thread(void* p)
+    {
+        auto bound = (internal::bind_type<Function, Args...>*)p;
+
+        bound->operator ()();
+    }
+
+protected:
+    thread(TaskHandle_t th) : th(th) {}
+
+public:
+    template <class Function, class ... Args>
+    thread(Function&& f, Args&&... args)
+    {
+        internal::bind_type<Function, Args...> binding(
+            std::move(f), 
+            std::forward<Args>(args)...);
+
+        th = xTaskCreate(start_thread<Function, Args...>, 
+            "test_task", 4096, NULL, 4, &th);
+    }
+
+    ~thread()
+    {
+        // FIX: though this is std lib behavior, some people
+        // suggest you should issue vTaskDelete from within the running
+        // task itself
+        vTaskDelete(th);
+    }
+    
+
+    typedef TaskHandle_t native_handle_type;
+    // this will map to xTaskNumber from xTASK_STATUS
+    // turns out this is only available from debug call to vTaskGetInfo
+    //typedef UBaseType_t id;
+    typedef native_handle_type id;
+
+    native_handle_type native_handle()
+    {
+        return th;
+    }
+
+    id get_id() const
+    {
+        return th;
+    }
+};
+
+namespace this_thread {
 
 // untested, not yet compiled, but everything is in place so I expect this to work with little
 // effort
@@ -16,4 +80,11 @@ void sleep_for( const chrono::duration<Rep, Period>& sleep_duration )
     vTaskDelay(count);
 }
 
-}}
+thread::id get_id()
+{
+    return xTaskGetCurrentTaskHandle();
+}
+
+}
+
+}
