a7887895c30af2217446ce4cf2d4246dc6703a17
Malachi Burke <malachi.burke@gmail.com>
2018-09-10 18:47:03 -0700

    Needs very explicit namespacing for some reason

--- src/estd/port/asf/chrono.h
+++ src/estd/port/asf/chrono.h
@@ -9,8 +9,8 @@ namespace chrono {
 // dummy definition for now for native clock
 struct asf_clock
 {
-    typedef estd::internal::milli_rep rep;
-    typedef estd::milli period;
+    typedef ::estd::internal::milli_rep rep;
+    typedef ::estd::milli period;
 
     typedef internal::estd_chrono::duration<rep, period> duration;
     typedef internal::estd_chrono::time_point<asf_clock> time_point;
