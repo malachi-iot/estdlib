97d6cdbec2b44c5cc1d10a1914a53912f33abf0d
Malachi Burke <malachi.burke@gmail.com>
2019-12-15 00:46:36 -0800

    Building out 'byte' from C++17
    Can't quite do all the constexpr stuff, still useful anyway

--- src/estd/cstddef.h
+++ src/estd/cstddef.h
@@ -10,21 +10,67 @@ enum class byte : unsigned char {};
 typedef unsigned char byte;
 #endif
 
+// TODO: Only allow overloads which conform to is_integral, as per spec
+
 template <class IntegerType>
 CONSTEXPR IntegerType to_integer(byte b) noexcept
 {
-    // TODO: Only allow overloads which conform to is_integral, as per spec
     //is_integral<IntegerType>::value;
     return IntegerType(b);
 }
 
 
-/* TODO: Figure out how to do this; not critical for my own uses yet
+// NONSTANDARD
+// Because most of us aren't running C++17 yet with relaxed rules to do the
+// canonical byte v={0} or similar, so do this in the meantime
+template <class IntegerType>
+CONSTEXPR byte to_byte(IntegerType value) noexcept
+{
+    return (byte) value;
+}
+
+
+template <class IntegerType>
+CONSTEXPR byte operator <<(byte b, IntegerType shift) noexcept
+{
+    return (byte)((unsigned char)b << shift);
+    //return b;
+}
+
 template <class IntegerType>
 CONSTEXPR byte& operator >>=(byte& b, IntegerType shift) noexcept
 {
-    return (b = ((unsigned char)b) >> shift);
-} */
+    //return ((unsigned char&)b) >>= shift;
+    return b = (byte)(((unsigned char)b) >> shift);
+    //return b;
+}
+
+
+template <class IntegerType>
+CONSTEXPR byte& operator <<=(byte& b, IntegerType shift) noexcept
+{
+    return b = b << shift;
+    //return b = (byte)(((unsigned char)b) << shift);
+    //return b;
+}
+
+
+inline CONSTEXPR byte operator|(byte l, byte r) noexcept
+{
+    return to_byte(static_cast<unsigned int>(l) | static_cast<unsigned int>(r));
+}
+
+inline
+#ifdef FEATURE_CPP_CONSTEXPR_METHOD
+constexpr
+#else
+const
+#endif
+byte& operator|=(byte& l, byte r) noexcept
+{
+    return l = l | r;
+}
+
 
 }
 
--- test/catch/cstddef-test.cpp
+++ test/catch/cstddef-test.cpp
@@ -8,9 +8,27 @@ TEST_CASE("cstddef")
 {
     SECTION("byte")
     {
-        /*
-        estd::byte val;
+        estd::byte val = to_byte(2);
 
-        val >>= 10; */
+        SECTION(">>=")
+        {
+            val >>= 1;
+
+            REQUIRE(to_integer<int>(val) == 1);
+        }
+        SECTION("<<=")
+        {
+            val <<= 2;
+
+            REQUIRE(to_integer<int>(val) == 8);
+        }
+        SECTION("|=")
+        {
+            estd::byte other_val = to_byte(4);
+
+            val |= other_val;
+
+            REQUIRE(to_integer<int>(val) == 6);
+        }
     }
 }
