282b11435cb437128232db5353f77125707e6b57
Malachi Burke <malachi.burke@gmail.com>
2020-01-19 20:19:16 -0800

    Revised test to more easily see results

--- test/arduino/string/src/string-example.cpp
+++ test/arduino/string/src/string-example.cpp
@@ -5,19 +5,23 @@
 void setup()
 {
     Serial.begin(115200);
-
-    estd::layer1::string<64> name;
-
-    name = "Mickey";
-
-    Serial.println(F("hi"));
-    
-    Serial << "Hello: ";
-    Serial << name << F(" Mouse");
 }
 
 
 void loop()
 {
+    static int counter = 0;
 
+    estd::layer1::string<64> name;
+
+    name = "Mickey";
+    name += ' ';
+    name += "Mouse";
+
+    Serial << "Hello: ";
+    Serial << name << F(" - counter=") << counter++;
+
+    Serial.println(F("!!"));
+    
+    delay(1000);
 }
