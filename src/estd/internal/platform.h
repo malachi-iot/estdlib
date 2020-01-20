7c6c8072b81f7a7217efa4bddd4cbdb2f5ccb2b7
Malachi Burke <malachi.burke@gmail.com>
2020-01-19 20:18:15 -0800

    More inclusive of Arduino esp environemnts for STD availability

--- src/estd/internal/platform.h
+++ src/estd/internal/platform.h
@@ -157,12 +157,16 @@
 
 // some platforms have c++98 onward but don't have STL headers.  Explicitly state when we *do*
 // have them
-#if !defined(ARDUINO) || defined(ESP8266)
+#if !defined(ARDUINO) || defined(ESP8266) || \
+    defined(ESP_PLATFORM)
+
 #define FEATURE_STD_ALGORITHM
 #define FEATURE_STD_CSTDDEF
+
 #if __ADSPBLACKFIN__
 #define FEATURE_STD_CLIMITS
 #endif
+
 #if __cplusplus >= 201103L
 // https://stackoverflow.com/questions/13642827/cstdint-vs-stdint-h indicates it's a c++11 feature, as
 // corroborated by https://en.cppreference.com/w/cpp/types/integer.  stackoverflow also indicates it's
@@ -170,12 +174,14 @@
 #define FEATURE_STD_CSTDINT
 #define FEATURE_STD_CASSERT
 #define FEATURE_STD_INITIALIZER_LIST
-#endif
+#endif  // end C++11
+
 #define FEATURE_STD_ITERATOR
 #define FEATURE_STD_MEMORY
 #define FEATURE_STD_STRING
 #define FEATURE_STD_TYPE_TRAITS
 #define FEATURE_STD_UTILITY
+
 #endif
 
 // Now with all the capabilities set up, do more complex platform support,
