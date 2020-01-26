349cd007305bcc7faf4f91f550b6eca7ee297062
Malachi Burke <malachi.burke@gmail.com>
2020-01-26 13:16:27 -0800

    Deferring clang repair for another time

--- src/estd/internal/type_traits.h
+++ src/estd/internal/type_traits.h
@@ -2,6 +2,9 @@
  *  @file
  *
  * Portion of type traits with no dependencies (except internal/platform.h)
+ * TODO: Document why this is needed so that we can structure raw vs internal properly.
+ * Until we do that, leave this in a half-refactored state so that we don't cause dependency
+ * issues accidentally
  */
 
 #pragma once
