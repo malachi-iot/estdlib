4a0e5dbdab47bcc1c642f9b8b146027cb5654008
Malachi Burke <malachi.burke@gmail.com>
2020-05-10 20:49:05 -0700

    Reducing chance of getting false success

--- test/unity/map.cpp
+++ test/unity/map.cpp
@@ -8,19 +8,19 @@
 
 void test_layer1_map()
 {
-    // copy/pasted from catch.hpp unit tests
+    // adapted from catch.hpp unit tests
     
     estd::layer1::map<int, int, 4> map =
     {
         { 1, 77 },
-        { 2, 78 },
-        { 3, 79 },
-        { 4, 80 }
+        { 3, 78 },
+        { 5, 79 },
+        { 7, 80 }
     };
 
-    auto result = map[2];
+    auto result = map[3];
 
-    TEST_ASSERT_EQUAL_HEX8(78, *result);
+    TEST_ASSERT_EQUAL(78, *result);
 }
 
 #ifdef ESP_IDF_TESTING
