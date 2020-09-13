8569f5fd8b1022d0d090e24d3e31ebf46a432703
Malachi Burke <malachi.burke@gmail.com>
2020-09-13 15:45:08 -0700

    Successful 'enum class' emulation

--- src/estd/system_error.h
+++ src/estd/system_error.h
@@ -5,6 +5,7 @@
 
 #include "internal/platform.h"
 
+// Not actually using yet, just to dogfood our custom version
 #ifdef FEATURE_STD_SYSTEM_ERROR
 #include <system_error>
 namespace estd {
@@ -16,29 +17,19 @@ namespace estd {
 
 namespace internal {
 
-struct enum_helper
+// To work around < C++11's lack of "enum class"
+// TEnumClass must always contain an enum called 'values'
+// TODO: Move this enum_helper elsewhere for easier discovery
+template <class TEnumClass, class TValue = int>
+struct enum_helper : TEnumClass
 {
 protected:
-    int value;
+    TValue value;
 
 public:
-    explicit enum_helper(int value) : value(value) {}
-};
-}
+    explicit enum_helper(TValue value) : value(value) {}
 
-// UNTESTED
-class errc : public internal::enum_helper
-{
-public:
-    explicit errc(int value) :
-        internal::enum_helper(value) {}
-    
-    enum values
-    {
-        address_family_not_supported = EAFNOSUPPORT,
-        invalid_argument = EINVAL,
-        result_out_of_range = ERANGE
-    };
+    typedef typename TEnumClass::values values;
 
     bool operator == (values compare_to) const
     {
@@ -49,6 +40,27 @@ public:
     {
         return value != compare_to;
     }
+};
+
+// manual "enum class", enum itself must always be called 'values'
+struct errc_enum
+{
+    enum values
+    {
+        address_family_not_supported = EAFNOSUPPORT,
+        invalid_argument = EINVAL,
+        result_out_of_range = ERANGE
+    };
+};
+
+}
+
+class errc : public internal::enum_helper<internal::errc_enum>
+{
+public:
+    explicit errc(int value) :
+        internal::enum_helper<internal::errc_enum>(value) {}
+    
 };
 
 #endif
