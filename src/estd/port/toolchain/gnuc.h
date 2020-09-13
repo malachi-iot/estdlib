a62d1c50bed30bbf0c2eb547d3b28988f370b5fd
Malachi Burke <malachi.burke@gmail.com>
2020-09-13 14:49:43 -0700

    More resilient attempts to verify/create __GNUC_PREREQ

--- src/estd/port/toolchain/gnuc.h
+++ src/estd/port/toolchain/gnuc.h
@@ -13,22 +13,31 @@
 # endif
 #endif
 
-
 // First, see if we need to explicitly include any of the stuff in
 // GLIBC's (or similar, like newlib) features.h
 #if !defined(__GNUC_PREREQ)
-// If so, try to get it
+
+#ifdef __AVR_LIBC_VERSION__
+// Won't have <features.h>, may need to manually create __GNUC_PREREQ
+// though ASF seems to have it somewhere
+#elif defined(__NEWLIB__)
+// TODO: Newlib does have it I believe in <sys/features.h>
+// Until we verify that, manually create it
 // Generally, __GLIBC__ is actually defined *inside* features.h so this might
 // be a partially broken test
-#if defined(__linux__) || defined(__GLIBC__)
+#elif defined(__linux__) || defined(__GLIBC__)
 // Only a linux/glibc thing:
 // https://stackoverflow.com/questions/259248/how-to-test-the-current-version-of-gcc-at-compile-time
 // GLIBC detection gleaned from
 // http://web.mit.edu/freebsd/head/contrib/wpa/src/utils/common.h
 #include <features.h>
-#else
+#endif
+
+// If after all of above we still don't have one, manually create it
+#if !defined(__GNUC_PREREQ)
 #define __GNUC_PREREQ(major, minor) (__GNUC__ > major || (__GNUC__ == major && __GNUC__MINOR__ >= minor))
 #endif
+
 #endif
 
 
