401e71211f4c523a08a076b146691d3e0638c815
Malachi Burke <malachi.burke@gmail.com>
2020-09-16 10:28:37 -0700

    More alignment fiddling.  Shelving the rest for now

--- test/unity/align.cpp
+++ test/unity/align.cpp
@@ -1,19 +1,22 @@
 #include "unit-test.h"
 
 #include <estd/type_traits.h>
+#include <estd/array.h>
 
 using namespace estd;
 
-// TODO: VisualDSP does have some sort of _Alignas keyword, but it seems to differ from
-// https://en.cppreference.com/w/c/language/_Alignas
+// VisualDSP has no alignas or _Alignas keyword
 
 static void test_align_1()
 {
     typedef typename aligned_storage<sizeof(TestStruct), __alignof__(TestStruct)>::type data_type;
 
-    data_type data;
-
     TEST_ASSERT_EQUAL_INT(sizeof(int), sizeof(data_type));
+}
+
+static void test_align_2()
+{
+    experimental::aligned_storage_array<TestStruct, 10> a;
 }
 
 #ifdef ESP_IDF_TESTING
