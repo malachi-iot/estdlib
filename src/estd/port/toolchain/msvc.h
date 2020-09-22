52c2f6d56d1b3217c473d9f6174ae63065a04b9a
Malachi Burke <malachi.burke@gmail.com>
2020-09-22 12:27:00 -0700

    Moving towards GNU-style bitness indicators
    Ala https://sourceforge.net/p/predef/wiki/DataModels/

--- src/estd/port/toolchain/msvc.h
+++ src/estd/port/toolchain/msvc.h
@@ -2,10 +2,17 @@
 
 #define __alignof__(T) __alignof(T)
 
+// DEBT: Phase out ESTD_ARCH_BITNESS for more gnu-emulated LP64/ILP32
 #if _WIN32 || _WIN64
 #if _WIN64
 #define ESTD_ARCH_BITNESS   64
+#ifndef __LP64__
+#define __LP64__
+#endif
 #else
 #define ESTD_ARCH_BITNESS   32
+#ifndef __ILP32__
+#define __ILP32__
+#endif
 #endif
 #endif
--- src/estd/port/toolchain/visualdsp.h
+++ src/estd/port/toolchain/visualdsp.h
@@ -1,3 +1,10 @@
 #pragma once
 
 #include "gnuc.h"
+
+// Using https://sourceforge.net/p/predef/wiki/DataModels/ as reference as well
+// as "VisualDSP++ 4.5 C/C++ Compiler and Library Manual" Table 1-35
+// DEBT: Isolate this to Blackfin only
+#ifndef __ILP32__
+#define __ILP32__
+#endif
