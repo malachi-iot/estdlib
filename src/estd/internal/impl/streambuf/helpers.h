3d8cd85f33dfda88e24f2d61192c3ec8df595cec
Malachi Burke <malachi.burke@gmail.com>
2020-09-24 04:28:13 -0700

    Adding unused experimental code

---
+++ src/estd/internal/impl/streambuf/helpers.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include "../streambuf.h"
+
+namespace estd { namespace internal { namespace impl { namespace experimental {
+
+template <class TStreambuf>
+typename TStreambuf::int_type uflow(TStreambuf* sb)
+{
+    typedef typename TStreambuf::traits_type traits_type;
+    typedef typename TStreambuf::int_type int_type;
+    int_type ch = sb->underflow();
+
+    if(ch != traits_type::eof())
+        sb->gbump(1);
+
+    return ch;
+}
+
+template <class TStreambuf>
+typename TStreambuf::int_type seekoff(TStreambuf* sb)
+{
+    return 0;
+}
+
+}}}}
