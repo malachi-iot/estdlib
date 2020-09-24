93362e982af851f6e8a9c803fa47bdcb5b0121c9
Malachi Burke <malachi.burke@gmail.com>
2020-09-24 12:46:52 -0700

    Adding unfinished unit tests
    Just to ensure compilation works

--- test/unity/streambuf.cpp
+++ test/unity/streambuf.cpp
@@ -4,5 +4,36 @@
  * Current ESTD_FN_HAS_METHOD + function default template param approach a no go
  */
 
+#include "unit-test.h"
 
-//#include <estd/streambuf.h>
+#include <estd/streambuf.h>
+#include <estd/istream.h>
+#include <estd/ostream.h>
+
+static void test_ospanbuf()
+{
+    char buf[128];
+    estd::span<char> span(buf);
+
+    estd::experimental::ospanbuf os(span);
+}
+
+
+static void test_ispanbuf()
+{
+    char buf[] = "hi2u 1234";
+    estd::span<char> span(buf);
+    
+    estd::experimental::ispanbuf os(span);
+}
+
+
+
+#ifdef ESP_IDF_TESTING
+TEST_CASE("streambuf", "[streambuf]")
+#else
+void test_streambuf()
+#endif
+{
+    RUN_TEST(test_ospanbuf);
+}
