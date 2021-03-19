49a8df472b34edae2bdc9430b29b4c98894577f8
Malachi Burke <malachi.burke@gmail.com>
2021-03-18 17:30:44 -0700

    Very basic "real" unit test

--- test/catch/locale-test.cpp
+++ test/catch/locale-test.cpp
@@ -1,9 +1,19 @@
 #include <catch.hpp>
 
+#include <estd/locale.h>
+
+using namespace estd;
+
 TEST_CASE("locale")
 {
+    experimental::locale l;
+
+    SECTION("isspace")
+    {
+        REQUIRE(experimental::isspace(' ', l));
+    }
     SECTION("use_facet")
     {
-        
+
     }
 }
