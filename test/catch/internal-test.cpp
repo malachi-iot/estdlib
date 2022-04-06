7677111e602b5fbd9c03626443ce37be52422fc3
Malachi Burke <malachi.burke@gmail.com>
2022-04-06 16:35:34 -0700

    Prepping value_evaporator tests

---
+++ test/catch/internal-test.cpp
@@ -0,0 +1,14 @@
+#include <catch.hpp>
+
+#include "estd/internal/value_evaporator.h"
+#include "test-data.h"
+
+using namespace estd::test;
+
+TEST_CASE("Internal tests", "[internal]")
+{
+    SECTION("value_evaporator")
+    {
+        //estd::internal::value_evaporator<Dummy, true, Dummy, Dummy()> ve;
+    }
+}
