fb35a0015da4f817056a809399c5fb8e89b0a025
Malachi Burke <malachi.burke@gmail.com>
2022-04-08 13:35:21 -0700

    Adding boilerplate-ish freertos & esp-idf chrono tests

--- test/unity/chrono.cpp
+++ test/unity/chrono.cpp
@@ -37,11 +37,39 @@ static void test_chrono_subtract()
 #ifdef FEATURE_STD_CHRONO
 static void test_steady_clock()
 {
-    estd::chrono::steady_clock c;
+    typedef estd::chrono::steady_clock clock_type;
+    typedef clock_type::time_point time_point;
 
-    estd::chrono::steady_clock::time_point n = c.now();
+    clock_type c;
+
+    //time_point min = time_point::min();
+    time_point n = c.now();
+    /*
+    estd::chrono::milliseconds n2 = 
+        // DEBT: Ferret out proper estd version of this
+        estd::chrono::duration_cast<estd::chrono::milliseconds>(n); */
 }
 #endif
+
+#ifdef ESTD_FREERTOS
+static void test_freertos_clock()
+{
+    estd::chrono::freertos_clock c;
+
+    estd::chrono::freertos_clock::time_point n = c.now();
+}
+#endif
+
+#ifdef ESTD_SDK_IDF
+static void test_esp_idf_clock()
+{
+    estd::chrono::esp_clock c;
+
+    estd::chrono::esp_clock::time_point n = c.now();
+}
+#endif
+
+#include <chrono>
 
 #ifdef ESP_IDF_TESTING
 TEST_CASE("chrono tests", "[chrono]")
@@ -54,5 +82,13 @@ void test_chrono()
 #ifdef FEATURE_STD_CHRONO
     RUN_TEST(test_steady_clock);
 #endif
+#ifdef ESTD_FREERTOS
+    RUN_TEST(test_freertos_clock);
+#endif
+#ifdef ESTD_SDK_IDF
+    RUN_TEST(test_esp_idf_clock);
+#endif
+
+    std::chrono::steady_clock c;
 }
 
