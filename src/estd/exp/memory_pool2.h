caca53a43c84a6699db85d61512c0a8e7cd02a9c
Malachi Burke <malachi.burke@gmail.com>
2022-04-13 23:12:02 -0700

    Experimenting with handle/lockable memory allocator

--- src/estd/exp/memory_pool2.h
+++ src/estd/exp/memory_pool2.h
@@ -120,6 +120,270 @@ public:
 
 }; */
 
+enum maintenance_mode
+{
+    uninitialized,
+    compact,
+    move
+};
+
+
+// Doesn't belong in memory_pool area - we can get away with it for now since it's all experimental code
+template <unsigned N, unsigned N_Handles = (N >> 6)>
+struct virtual_memory
+{
+    maintenance_mode mode = maintenance_mode::uninitialized;
+
+    maintenance_mode state() const { return mode; }
+
+    typedef unsigned char byte_type;
+    typedef unsigned size_type;
+    typedef unsigned handle_index;
+    typedef byte_type* pointer;
+    typedef unsigned index_type;
+
+    byte_type storage[N];
+
+    struct handle
+    {
+        pointer data;
+        size_type chunk_size;
+
+        handle() { data = NULLPTR; }
+    };
+
+    // available space
+    struct item
+    {
+        item* next;
+        // tracks the size of free memory, including space occupied by this item
+        size_type chunk_size;
+    };
+
+    // occupied space
+    handle handles[N_Handles];
+
+    item* free_head;
+
+    union
+    {
+        struct
+        {
+            item* evaluating;
+            item* evaluating_prev;
+            size_type items_processed;
+
+        } compact_state;
+
+        struct
+        {
+
+        } move_state;
+    };
+
+
+    virtual_memory()
+    {
+        free_head = new (&storage[0]) item;
+        free_head->next = NULLPTR;
+        free_head->chunk_size = N;
+    }
+
+    handle* get_free_handle(index_type& index)
+    {
+        for(index_type i = 0; i < N_Handles; i++)
+        {
+            handle& h = handles[i];
+
+            if(h.data == NULLPTR)
+            {
+                index = i;
+                return &h;
+            }
+        }
+
+        return NULLPTR;
+    }
+
+    handle_index allocate(size_type size)
+    {
+        item* i = free_head,
+            *prev;
+
+        // TODO: coerce size to be aligned so that allocations and frees also have aligned size
+
+        // minimum size is 'item' size so that fragments have enough space to track themselves
+        if(size < sizeof(item))
+            size = sizeof(item);
+
+        while(i != NULLPTR)
+        {
+            handle_index hi;
+            if(i->chunk_size == size)
+            {
+                prev->next = i->next;
+
+                handle* h = get_free_handle(hi);
+                h->data = (pointer) i;
+                h->chunk_size = size;
+
+                return hi;
+            }
+            else if(i->chunk_size > size)
+            {
+                handle* h = get_free_handle(hi);
+                h->data = ((pointer) i) + (i->chunk_size - size);
+                h->chunk_size = size;
+
+                i->chunk_size -= size;
+
+                return hi;
+            }
+            else
+            {
+
+            }
+
+            prev = i;
+            i = i->next;
+        }
+
+        return -1;
+    }
+
+    void free(index_type h_index)
+    {
+        handle& h = handles[h_index];
+
+        item* i = (item*)h.data;
+
+        i->chunk_size = h.chunk_size;
+        i->next = free_head;
+
+        h.data = NULLPTR;
+
+        free_head = i;
+    }
+
+    size_type total_free(size_type* node_counter = NULLPTR) const
+    {
+        item* i = free_head;
+        size_type count = 0;
+
+        if(node_counter != NULLPTR) *node_counter = 0;
+
+        while(i != NULLPTR)
+        {
+            if(node_counter != NULLPTR) ++(*node_counter);
+
+            count += i->chunk_size;
+            i = i->next;
+        }
+
+        return count;
+    }
+
+    bool maint_absorb(item* i_physical_0, item* i_physical_1)
+    {
+        // if physical ptr diff is same as item# 1 chunk size
+        if((pointer)i_physical_1 - (pointer)i_physical_0 == i_physical_0->chunk_size)
+        {
+            ++compact_state.items_processed;
+
+            // do merge/absorb operation
+
+            i_physical_0->next = i_physical_1->next;
+            i_physical_0->chunk_size += i_physical_1->chunk_size;
+
+            return true;
+        }
+        else
+            return false;
+    }
+
+    /// Walk the heap and bubble sort 'next' pointers which are out of order
+    /// and if we encounter contiguous free space along the way, merge 'item' together
+    bool maint_compact()
+    {
+        item* i = compact_state.evaluating;
+        item* i_prev = compact_state.evaluating_prev;
+
+        if(i->next == NULLPTR) return false;
+
+        // if next free node is positioned before this one
+        if(i->next < i)
+        {
+            ++compact_state.items_processed;
+
+            item* i_physical_0 = i->next;
+            item* i_physical_1 = i;
+            // physical #2 may or may not be physically after #1, but we'll
+            // decide that on the next iteration of maint_compact
+            item* i_physical_2_assumed = i_physical_0->next;
+
+            i_physical_0->next = i_physical_1;
+            i_physical_1->next = i_physical_2_assumed;
+
+            if(i_prev == NULLPTR)
+            {
+                // this implies i is free_head
+                free_head = i_physical_0;
+            }
+            else
+                i_prev->next = i_physical_0;
+
+            if(maint_absorb(i_physical_0, i_physical_1))
+                i = i_physical_1;
+        }
+        else
+        {
+            item* i_physical_0 = i;
+            item* i_physical_1 = i->next;
+
+            if(maint_absorb(i_physical_0, i_physical_1))
+                i = i_physical_1;
+        }
+
+        compact_state.evaluating_prev = i;
+        compact_state.evaluating = i->next;
+
+        return true;
+    }
+
+    void maint_move()
+    {
+
+    }
+
+    void maint()
+    {
+        switch(mode)
+        {
+            case maintenance_mode::uninitialized:
+                mode = maintenance_mode::compact;
+                compact_state.evaluating = free_head;
+                compact_state.evaluating_prev = NULLPTR;
+                mode = maintenance_mode::compact;
+                maint();
+                break;
+
+            case maintenance_mode::compact:
+                compact_state.items_processed = 0;
+                maint_compact();
+                if(compact_state.items_processed == 0)
+                {
+                    mode = maintenance_mode::move;
+                }
+                break;
+
+            case maintenance_mode::move:
+                maint_move();
+                mode = maintenance_mode::uninitialized;
+                break;
+        }
+    }
+};
+
 }
 
 }}
--- test/catch/experimental-test.cpp
+++ test/catch/experimental-test.cpp
@@ -461,15 +461,81 @@ TEST_CASE("experimental tests")
             using namespace estd;
             using namespace estd::experimental;
 
-            typedef array<byte, 32> value_type;
+            SECTION("pool")
+            {
+                typedef array<byte, 32> value_type;
 
-            v2::memory_pool<value_type, 10, v2::pool_types::linked_list> pool;
+                v2::memory_pool<value_type, 10, v2::pool_types::linked_list> pool;
 
-            REQUIRE(pool.count_free() == 10);
-            value_type* v = pool.allocate();
-            REQUIRE(pool.count_free() == 9);
-            pool.deallocate(v);
-            REQUIRE(pool.count_free() == 10);
+                REQUIRE(pool.count_free() == 10);
+                value_type* v = pool.allocate();
+                REQUIRE(pool.count_free() == 9);
+                pool.deallocate(v);
+                REQUIRE(pool.count_free() == 10);
+            }
+            SECTION("virtual allocator")
+            {
+                constexpr int pool_size = 1024;
+                v2::virtual_memory<pool_size, 20> v;
+                unsigned node_counter;
+
+                SECTION("allocate 1 item")
+                {
+                    int index1 = v.allocate(10);
+
+                    v.free(index1);
+
+                    REQUIRE(v.total_free(&node_counter) == pool_size);
+                    REQUIRE(node_counter == 2);
+                }
+                SECTION("allocate 2 items")
+                {
+                    int index1 = v.allocate(10);
+                    int index2 = v.allocate(20);
+
+                    v.free(index1);
+                    v.free(index2);
+
+                    REQUIRE(v.total_free(&node_counter) == pool_size);
+                    REQUIRE(node_counter == 3);
+                }
+                SECTION("zero maint")
+                {
+                    v.maint();
+
+                    REQUIRE(v.total_free(&node_counter) == pool_size);
+                    REQUIRE(node_counter == 1);
+                }
+                SECTION("1 item maint")
+                {
+                    int index1 = v.allocate(10);
+
+                    v.free(index1);
+
+                    v.maint();
+
+                    REQUIRE(v.total_free(&node_counter) == pool_size);
+                    REQUIRE(node_counter == 1);
+                }
+                SECTION("2 item maint")
+                {
+                    int index1 = v.allocate(10);
+                    int index2 = v.allocate(20);
+
+                    v.free(index1);
+                    v.free(index2);
+
+                    // FIX: problematic
+                    do
+                    {
+                        v.maint();
+                    }
+                    while(v.state() != v2::maintenance_mode::uninitialized);
+
+                    REQUIRE(v.total_free(&node_counter) == pool_size);
+                    //REQUIRE(node_counter == 1);
+                }
+            }
         }
     }
     SECTION("instance wrapper")
