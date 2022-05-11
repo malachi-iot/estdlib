b99bc2e932b45ae22f3f8e5aec1e9ab1138adb1f
Malachi Burke <malachi.burke@gmail.com>
2022-05-11 10:42:11 -0700

    Windows now compiles under CLion
    Unit tests fail under some platform specific looking scenarios

--- src/estd/port/toolchain.h
+++ src/estd/port/toolchain.h
@@ -12,6 +12,13 @@
 #include "toolchain/msvc.h"
 #endif
 
+// As per
+// https://stackoverflow.com/questions/17493759/how-can-i-detect-g-and-mingw-in-c-preprocessor
+// this picks up 64 bit one also
+#if defined(__MINGW32__)
+#include "toolchain/mingw.h"
+#endif
+
 // NOTE: May just be identical to gnuc, but rolling distinct for now just incase
 // we wanna add something
 #if defined(__ADSPBLACKFIN__)
