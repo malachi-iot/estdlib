0e9b607af791ddc44ae9412370eab7be2cf71c15
Malachi Burke <malachi.burke@gmail.com>
2022-06-28 11:46:33 -0700

    Commenting

--- src/estd/internal/fwd/chrono.h
+++ src/estd/internal/fwd/chrono.h
@@ -5,6 +5,9 @@ namespace estd {
 // DEBT: Would do estd::chrono::internal, but that collides with our estd_chrono trick in port area
 namespace internal {
 
+// Since C++20 epoch is midnight 1/1/1970 GMT, and is loosely considered standardized on that
+// prior to C++20.  I haven't found any API in the spec which actually reflects that though, so
+// we make our own
 template <class TClock>
 struct clock_traits;
 
