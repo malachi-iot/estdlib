1ed96e05dce43153f4c41f7aeca85aca8b7c1e93
Malachi Burke <malachi.burke@gmail.com>
2022-08-15 11:48:24 -0700

    Experimentation

---
+++ test/catch/SetUpConfigurations.cmake
@@ -0,0 +1,25 @@
+# Guidance from
+# https://stackoverflow.com/questions/31546278/where-to-set-cmake-configuration-types-in-a-project-with-subprojects
+# Eventual goal is to multi target to c++11, c++14, c++20, etc. c++03/98 will still be targeted in a separate
+# Project due to catch.hpp vs catch2.hpp
+
+# Be sure to move this out into the /tools folder (maybe /tools/cmake)
+
+if(NOT SET_UP_CONFIGURATIONS_DONE)
+    set(SET_UP_CONFIGURATIONS_DONE TRUE)
+
+    # No reason to set CMAKE_CONFIGURATION_TYPES if it's not a multiconfig generator
+    # Also no reason mess with CMAKE_BUILD_TYPE if it's a multiconfig generator.
+    get_property(isMultiConfig GLOBAL PROPERTY GENERATOR_IS_MULTI_CONFIG)
+    if(isMultiConfig)
+        set(CMAKE_CONFIGURATION_TYPES "Debug;Release;Profile" CACHE STRING "" FORCE) 
+    else()
+        if(NOT CMAKE_BUILD_TYPE)
+            message("Defaulting to release build.")
+            set(CMAKE_BUILD_TYPE Release CACHE STRING "" FORCE)
+        endif()
+        set_property(CACHE CMAKE_BUILD_TYPE PROPERTY HELPSTRING "Choose the type of build")
+        # set the valid options for cmake-gui drop-down list
+        set_property(CACHE CMAKE_BUILD_TYPE PROPERTY STRINGS "Debug;Release;Profile")
+    endif()
+endif()
