05ee0191b5adf025ac1e03c8f7a1982e1469cc4a
Malachi Burke <malachi.burke@gmail.com>
2022-08-19 22:02:15 -0700

    Formalizing competing function implementations into compile-time impl
    Since we need more experimentation to decide which is best, but also need to
    start depending on the code now.

--- src/estd/functional.h
+++ src/estd/functional.h
@@ -182,34 +182,44 @@ struct model_base
 template <typename T, bool nullable = true, class TAllocator = monostate>
 class function;
 
-// TImpl currently experimental
-template <typename T, class TImpl = void>
-class function_base;
-
 struct function_base_tag {};
 
+namespace impl {
+
+// Since we prefer 'fnptr1', but can't discount the value of
+// 'virtual', formalizing different techniques into impls here
+// DEBT: May need to wrap these in a provider so that TResult(TArgs...) doesn't interfere too
+// much with us switching to different impls
+
+template <typename T>
+struct function_fnptr1;
+
+template <typename T>
+struct function_fnptr2;
+
+template <typename T>
+struct function_virtual;
 
 template <typename TResult, typename... TArgs>
-class function_base<TResult(TArgs...)> : public function_base_tag
+struct function_fnptr1<TResult(TArgs...)>
 {
-protected:
     // function pointer approach works, but if we have to add in a virtual destructor
     // (which is likely) then we are faced with incurring that overhead anyway so might
     // switch over to virtual operator() in that case
-    struct concept_fnptr1
+    struct model_base
     {
-        typedef TResult (concept_fnptr1::*function_type)(TArgs&&...);
-        typedef void (*deleter_type)(concept_fnptr1*);  // EXPERIMENTAL
+        typedef TResult (model_base::*function_type)(TArgs&&...);
+        typedef void (*deleter_type)(model_base*);  // EXPERIMENTAL
 
         const function_type f;
 
-        concept_fnptr1(function_type f) : f(f) {}
+        model_base(function_type f) : f(f) {}
 
-        concept_fnptr1(const concept_fnptr1& copy_from) = default;
+        model_base(const model_base& copy_from) = default;
         // just like concept_fnptr2, default move constructor somehow
         // results in make_inline2 leaving f uninitialized
         //concept_fnptr1(concept_fnptr1&& move_from) = default;
-        concept_fnptr1(concept_fnptr1&& move_from) :
+        model_base(model_base&& move_from) :
             f(move_from.f)
         {}
 
@@ -221,13 +231,13 @@ protected:
 
 
     template <typename F>
-    struct model_fnptr1 : concept_fnptr1
+    struct model : model_base
     {
-        typedef concept_fnptr1 base_type;
+        typedef model_base base_type;
 
         //template <typename U>
-        model_fnptr1(F&& u) :
-            base_type(static_cast<typename base_type::function_type>(&model_fnptr1::exec)),
+        model(F&& u) :
+            base_type(static_cast<typename base_type::function_type>(&model::exec)),
             f(std::forward<F>(u))
         {
         }
@@ -239,22 +249,27 @@ protected:
             return f(std::forward<TArgs>(args)...);
         }
     };
+};
 
+
+template <typename TResult, typename... TArgs>
+struct function_fnptr2<TResult(TArgs...)>
+{
     // this is a slightly less fancy more brute force approach to try to diagose esp32
     // woes
-    struct concept_fnptr2
+    struct model_base
     {
         typedef TResult (*function_type)(void*, TArgs&&...);
 
         const function_type _f;
 
-        concept_fnptr2(function_type f) : _f(f) {}
+        model_base(function_type f) : _f(f) {}
 
-        concept_fnptr2(const concept_fnptr2& copy_from) = default;
+        model_base(const model_base& copy_from) = default;
         // DEBT: For some reason ESP32's default move constructor
         // doesn't initialize _f
         //concept_fnptr2(concept_fnptr2&& move_from) = default;
-        concept_fnptr2(concept_fnptr2&& move_from) :
+        model_base(model_base&& move_from) :
             _f(std::move(move_from._f))
         {}
 
@@ -265,12 +280,12 @@ protected:
     };
 
     template <typename F>
-    struct model_fnptr2 : concept_fnptr2
+    struct model : model_base
     {
-        typedef concept_fnptr2 base_type;
+        typedef model_base base_type;
 
-        model_fnptr2(F&& u) :
-            base_type(static_cast<typename base_type::function_type>(&model_fnptr2::__exec)),
+        model(F&& u) :
+            base_type(static_cast<typename base_type::function_type>(&model::__exec)),
             f(std::forward<F>(u))
         {
         }
@@ -294,22 +309,27 @@ protected:
 
         static TResult __exec(void* _this, TArgs&&...args)
         {
-            auto __this = ((model_fnptr2*)_this);
+            auto __this = ((model*)_this);
 
             return __this->f(std::forward<TArgs>(args)...);
         }
     };
+};
 
-    struct concept_virtual
+
+template <typename TResult, typename... TArgs>
+struct function_virtual<TResult(TArgs...)>
+{
+    struct model_base
     {
         virtual TResult _exec(TArgs&&...args) = 0;
-        virtual ~concept_virtual() = default;
+        virtual ~model_base() = default;
     };
 
     template <class F>
-    struct model_virtual : concept_virtual
+    struct model : model_base
     {
-        model_virtual(F&& u) :
+        model(F&& u) :
             f(std::forward<F>(u))
         {
         }
@@ -326,28 +346,33 @@ protected:
             return f(std::forward<TArgs>(args)...);
         }
     };
+};
 
-    // DEBT: Need to change name from concept, I suspect this will cause
-    // c++20 issues
+}
+
+
+// DEBT: Put this out into a fwd area
+template <typename T, class TImpl = impl::function_fnptr1<T> >
+class function_base;
+
+
+// DEBT: Put this out into a 'detail' namespace, likely changing name to
+// 'function'
+template <typename TResult, typename... TArgs, class TImpl>
+class function_base<TResult(TArgs...), TImpl> : public function_base_tag
+{
+protected:
+    typedef TImpl impl_type;
+
     // We like this way best, but due to [1] it may not be viable.
     // However, they say that:
     // "Deleting a polymorphic object without a virtual destructor
     //  is permitted if the object is referenced by a pointer to its
     //  class, rather than via a pointer to a class it inherits from."
     // This might be possible with a manual function pointer deleter.
-    typedef concept_fnptr1 model_base;
+    typedef typename impl_type::model_base model_base;
     template <class F>
-    using model = model_fnptr1<F>;
-
-    /*
-    typedef concept_fnptr2 concept;
-    template <class F>
-    using model = model_fnptr2<F>; */
-
-    /*
-    typedef concept_virtual concept;
-    template <class F>
-    using model = model_virtual<F>; */
+    using model = typename impl_type::template model<F>;
 
     // DEBT: 'function' constructors need this to not be const, for now
     //concept* const m;
