08f08217c8a883669c0210b30e64cbcffa780b5c
Malachi Burke <malachi.burke@gmail.com>
2022-09-07 12:32:48 -0700

    Experimentation

--- src/estd/internal/raw/cstddef.h
+++ src/estd/internal/raw/cstddef.h
@@ -102,9 +102,28 @@ byte& operator|=(byte& l, byte r) NOEXCEPT
 #if __cplusplus < 201103L
 namespace std {
 
+// Possible guidance:
+// https://stackoverflow.com/questions/44517556/how-to-define-our-own-nullptr-in-c98
+
 #ifdef __GNUC__
 //typedef void* nullptr_t;
-typedef typeof(NULLPTR) nullptr_t;
+class nullptr_t
+{
+public:
+    template<class T>          /* convertible to any type       */
+    operator T*() const        /* of null non-member            */
+    { return 0; }           /* pointer...                    */
+
+    template<class C, class T> /* or any type of null           */
+    operator T C::*() const /* member pointer...             */
+    { return 0; }
+
+private:
+    void operator&() const;    /* Can't take address of nullptr */
+
+};
+
+//typedef typeof(NULLPTR) nullptr_t;
 //struct nullptr_t {};
 #endif
 
