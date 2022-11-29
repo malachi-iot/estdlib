0e77af3f4b06a6bab13040fbf644658fedab987c
Malachi Burke <malachi.burke@gmail.com>
2022-11-29 14:28:55 -0800

    Ugly fixups for INTERFACE lib/FreeRTOS support

--- tools/cmake/rpi-pico.cmake
+++ tools/cmake/rpi-pico.cmake
@@ -3,10 +3,15 @@ message(DEBUG "estdlib: Raspberry Pi Pico mode")
 set(ESTD_AS_INTERFACE 1)
 
 if(DEFINED ENV{FREERTOS_KERNEL_PATH})
+    message(DEBUG "estdlib: Raspberry Pi Pico FreeRTOS")
+
     # Tested against v10.5.1
     include($ENV{FREERTOS_KERNEL_PATH}/portable/ThirdParty/GCC/RP2040/FreeRTOS_Kernel_import.cmake)
+    # DEBT: Brute force, would prefer to acquire property from estd scope
+    # Interface properties present special challenges
+    # https://stackoverflow.com/questions/68502038/custom-properties-for-interface-libraries
+    set(ESTD_OS_FREERTOS 2 CACHE INTERNAL "")
     # DEBT: '2' here means flag as FREERTOS but then rewrite as 1 or 
     # RTOS version number - not so clear
-    add_compile_definitions(ESTD_OS_FREERTOS=2)
-    set(ESTD_OS_FREERTOS 2)
+    add_compile_definitions(ESTD_OS_FREERTOS=${ESTD_OS_FREERTOS})
 endif()
