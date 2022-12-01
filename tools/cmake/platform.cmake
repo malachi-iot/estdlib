be9d2135ef2e8bbca9af6e6bc546983f96374d37
Malachi Burke <malachi.burke@gmail.com>
2022-12-01 15:32:01 -0800

    Repairing bug in INTERFACE sources
    Functionality springs to life and necessitates cmake_minimum to 3.13
    for target_sources to be happy
    
    Adding experimental define_property in hope of moving away from the CACHE junk

--- tools/cmake/platform.cmake
+++ tools/cmake/platform.cmake
@@ -1,3 +1,8 @@
+# ++ Experimental
+define_property(GLOBAL PROPERTY FEATURE_ESTD_INTERFACE)
+define_property(GLOBAL PROPERTY ESTD_OS_FREERTOS)
+# --
+
 if(NOT DEFINED FEATURE_ESTD_FREERTOS)
     message(DEBUG "estd: defaulting FEATURE_ESTD_FREERTOS to 1")
     set(FEATURE_ESTD_FREERTOS 1)
