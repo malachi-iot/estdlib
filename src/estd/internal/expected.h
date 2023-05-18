9fb1926236e6b41921f9231c765b6c334682e35c
Malachi Burke <malachi.burke@gmail.com>
2023-05-17 21:50:37 -0700

    Cleanup

--- src/estd/internal/expected.h
+++ src/estd/internal/expected.h
@@ -7,6 +7,11 @@
 
 #include "raw/expected.h"
 
+// DEBT: At first we made an effort to be c++03 compliant, but now things like
+// 'are_trivial' and 'variant_storage' are very much c++11 onward.  So, either
+// make THEM also c++03 compliant, or remove the sprinkling of __cpp_variadic_templates
+// (and friends) checking
+
 namespace estd { namespace internal {
 
 // Doesn't need to play with uninitialized storage
@@ -41,52 +46,18 @@ public:
 };
 
 
+// Silently promote void to monostate so that it registers as 'trivial'
 template <class T, class E>
-struct expected_is_trivial;
-
-template <class T, class E>
-using expected_is_trivial2 =
+using expected_is_trivial =
     are_trivial<conditional_t<is_void<T>::value, monostate, T>, E>;
 
 
-template <class T, class E, bool trivial =
-        //expected_is_trivial<T, E>::is_trivial>
-        expected_is_trivial2<T, E>::value>
-        //are_trivial<T, E>::value>
+template <class T, class E, bool trivial = expected_is_trivial<T, E>::value>
 class expected;
-
-template <class T, class E>
-struct expected_is_trivial
-{
-    // DEBT: Do this all with an integral constant, just having a brain
-    // fart how to do an or operation in that context
-    static CONSTEXPR bool is_trivial =
-#if FEATURE_ESTD_IS_TRIVIAL
-        estd::is_trivial<T>::value & estd::is_trivial<E>::value;
-#elif FEATURE_STD_TYPE_TRAITS
-        std::is_trivial<T>::value & std::is_trivial<E>::value;
-#else
-        false;
-#endif
-};
-
-template <class E>
-struct expected_is_trivial<void, E>
-{
-    static CONSTEXPR bool is_trivial =
-#if FEATURE_ESTD_IS_TRIVIAL
-        estd::is_trivial<E>::value;
-#elif FEATURE_STD_TYPE_TRAITS
-        std::is_trivial<E>::value;
-#else
-        false;
-#endif
-};
 
 // DEBT: We can't really use estd::layer1::optional here as E and T
 // overlap, but perhaps we can use a high-bit filter in controlled
 // circumstances
-
 
 // Trivial flavor
 template <class T, class E>
@@ -97,13 +68,12 @@ public:
     typedef E error_type;
 
 protected:
-    typedef T nonvoid_value_type;
+    typedef conditional_t<is_void<T>::value, monostate, T> nonvoid_value_type;
 
-    variant_storage2<T, E> storage;
+    variant_storage2<nonvoid_value_type, E> storage;
 
-    //ESTD_CPP_CONSTEXPR_RET
-    expected() :
-        storage(in_place_index_t<0>{}, T{}) {}
+    ESTD_CPP_CONSTEXPR_RET expected() :
+        storage(in_place_index_t<0>{}, nonvoid_value_type{}) {}
 
 #if __cpp_variadic_templates
     template <class... TArgs>
@@ -121,22 +91,21 @@ protected:
     ESTD_CPP_CONSTEXPR_RET expected(unexpect_t, const TE1& e) : error_(e) {}
 #endif
 
-    // DEBT: Bring this back
-    //ESTD_CPP_CONSTEXPR_RET
-    expected(const value_type& v) :
+    ESTD_CPP_CONSTEXPR_RET expected(const nonvoid_value_type& v) :
         storage(in_place_index_t<0>{}, v)
     {}
 
 public:
-    T& value() { return get<0>(storage); }
-    ESTD_CPP_CONSTEXPR_RET const T& value() const { return get<0>(storage); }
+    // DEBT: Deviates from spec when void is involved, returns monostate
+    nonvoid_value_type& value() { return get<0>(storage); }
+    ESTD_CPP_CONSTEXPR_RET const nonvoid_value_type& value() const { return get<0>(storage); }
 
     E& error() { return get<1>(storage); }
     ESTD_CPP_CONSTEXPR_RET const E& error() const { return get<1>(storage); }
 
-    const T& operator*() const { return value(); }
+    const nonvoid_value_type& operator*() const { return value(); }
 
-    expected& operator=(value_type&& v)
+    expected& operator=(nonvoid_value_type&& v)
     {
         // FIX: Should we be doing std::move here?  And if not, document why not
         value() = v;
@@ -144,24 +113,25 @@ public:
 };
 
 
-// Non trivial flavor
+// Non trivial flavor, mainly need this because we can't
+// do constexprs so easily with non trivial one
 template <class T, class E>
 class expected<T, E, false>
 {
     variant_storage<false, T, E> storage;
 
 protected:
-    typedef T nonvoid_value_type;
+    typedef conditional_t<is_void<T>::value, monostate, T> nonvoid_value_type;
 
 #if __cpp_variadic_templates
     template <class... TArgs>
-    explicit expected(in_place_t, TArgs&&...args) :
+    constexpr explicit expected(in_place_t, TArgs&&...args) :
         storage(in_place_index_t<0>{}, std::forward<TArgs>(args)...)
     {
     }
 
     template <class... TArgs>
-    explicit expected(unexpect_t, TArgs&&...args) :
+    constexpr explicit expected(unexpect_t, TArgs&&...args) :
         storage(in_place_index_t<1>{}, std::forward<TArgs>(args)...)
     {
     }
@@ -169,12 +139,12 @@ protected:
 
     // DEBT: Figure out how to make this and others work on initializing list line
     // so that we can constexpr it
-    explicit expected(const nonvoid_value_type& v) :
+    constexpr explicit expected(const nonvoid_value_type& v) :
         storage(in_place_index_t<0>{}, v)
     {
     }
 
-    explicit expected() :
+    constexpr explicit expected() :
         storage(in_place_index_t<0>{}, T{})
     {
     }
@@ -183,42 +153,13 @@ public:
     typedef T value_type;
     typedef E error_type;
 
-    T& value() { return get<0>(storage); }
-    const T& value() const { return get<0>(storage); }
-    E& error() { return get<1>(storage); }
-
-    const T& operator*() const { return value(); }
-};
-
-
-template <class E>
-class expected<void, E, true>
-{
-    variant_storage<true, monostate, E> storage;
-
-public:
-    typedef void value_type;
-    typedef estd::monostate nonvoid_value_type;
-    typedef E error_type;
-
-protected:
-    ESTD_CPP_DEFAULT_CTOR(expected)
-
-#if __cpp_variadic_templates
-    template <class... TArgs>
-    constexpr explicit expected(unexpect_t, TArgs&&...args) :
-        storage(in_place_index_t<1>{}, std::forward<TArgs>(args)...)
-    {}
-#else
-    template <class TE1>
-    ESTD_CPP_CONSTEXPR_RET expected(unexpect_t, const TE1& e) : base_type(e) {}
-#endif
-
-public:
-    static void value() { }
-
+    nonvoid_value_type& value() { return get<0>(storage); }
+    const nonvoid_value_type& value() const { return get<0>(storage); }
     E& error() { return get<1>(storage); }
     const E& error() const { return get<1>(storage); }
+
+    const nonvoid_value_type& operator*() const { return value(); }
 };
+
 
 }}
