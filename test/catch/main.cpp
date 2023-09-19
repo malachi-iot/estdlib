d16571de6d247df831b502be6beeaf1437534ff7
Malachi Burke <malachi.burke@gmail.com>
2023-09-19 11:09:52 -0700

    Moving away from submodule for Catch2.hpp

--- test/catch/main.cpp
+++ test/catch/main.cpp
@@ -4,7 +4,7 @@
 #define CATCH_CONFIG_MAIN
 #endif
 
-#include <catch.hpp>
+#include <catch2/catch.hpp>
 
 #include "test-data.h"
 
--- tools/cmake/setvars.cmake
+++ tools/cmake/setvars.cmake
@@ -1,3 +1,11 @@
 get_filename_component(ROOT_DIR ${CMAKE_CURRENT_LIST_DIR}/../.. ABSOLUTE)
 get_filename_component(TOOLS_DIR ${CMAKE_CURRENT_LIST_DIR} ABSOLUTE)
 set(ESTD_DIR ${ROOT_DIR}/src)
+
+include(FetchContent)
+
+# NOTE: Cache dir not a standard FetchContent mechanism
+set(FETCHCONTENT_CACHE_DIR $ENV{HOME}/.local/cache/cmake/proj)
+if(EXISTS ${FETCHCONTENT_CACHE_DIR})
+    set(FETCHCONTENT_BASE_DIR ${FETCHCONTENT_CACHE_DIR}/${PROJECT_NAME}-${PROJECT_VERSION})
+endif()
