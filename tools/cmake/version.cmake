1e847f6886179b65555afc31e09c75706bf11c68
Malachi Burke <malachi.burke@gmail.com>
2023-10-23 12:04:17 -0700

    Further automating version stamping

--- tools/cmake/version.cmake
+++ tools/cmake/version.cmake
@@ -1,7 +1,20 @@
+# Depends on setvars
+
 string(TOUPPER ${PROJECT_NAME} PROJECT_NAME_UPPER)
+
+set(WORKING_DIR ${CMAKE_CURRENT_LIST_DIR}/in)
 
 # DEBT: 'port' is not a great destination
 configure_file(
-	${CMAKE_CURRENT_LIST_DIR}/version.in.h
-	${CMAKE_CURRENT_SOURCE_DIR}/estd/port/version.h)
+    ${WORKING_DIR}/version.in.h
+    ${CMAKE_CURRENT_SOURCE_DIR}/estd/port/version.h)
 
+configure_file(
+    ${WORKING_DIR}/idf_component.in.yml
+    ${ROOT_DIR}/tools/esp-idf/components/estdlib/idf_component.yml)
+
+# For platformio
+configure_file(
+    ${WORKING_DIR}/library.in.json
+    ${ROOT_DIR}/library.json)
+	
