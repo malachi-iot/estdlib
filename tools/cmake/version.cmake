4ab809ccdb1b39532aaae6a8ac378bf49469bbcf
Malachi Burke <malachi.burke@gmail.com>
2023-10-23 12:31:33 -0700

    idf component mechanism doesn't like our tools/esp-idf approach

--- tools/cmake/version.cmake
+++ tools/cmake/version.cmake
@@ -9,9 +9,15 @@ configure_file(
     ${WORKING_DIR}/version.in.h
     ${CMAKE_CURRENT_SOURCE_DIR}/estd/port/version.h)
 
+# esp-idf: For internal testing
 configure_file(
     ${WORKING_DIR}/idf_component.in.yml
     ${ROOT_DIR}/tools/esp-idf/components/estdlib/idf_component.yml)
+
+# esp-idf: For general use
+configure_file(
+    ${WORKING_DIR}/idf_component.in.yml
+    ${ROOT_DIR}/idf_component.yml)
 
 # For platformio
 configure_file(
