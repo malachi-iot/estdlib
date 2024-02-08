4d9cac8fca55fdca6ad06bb65f3d9d7159e08732
Malachi Burke <malachi.burke@gmail.com>
2024-02-08 08:48:37 -0800

    Prep for https://github.com/malachi-iot/estdlib/issues/23

---
+++ tools/cmake/ext.cmake
@@ -0,0 +1,21 @@
+# 3rd party things
+
+# https://github.com/malachi-iot/estdlib/issues/23 
+# Mentioned also in https://github.com/malachi-iot/estdlib/issues/22
+
+option(ESTD_DRAGONBOX
+    "3rd party float->int lib used for float->string conversions"
+    false)
+
+if(ESTD_DRAGONBOX)
+    FetchContent_Declare(
+        dragonbox
+        GIT_REPOSITORY  https://github.com/jk-jeon/dragonbox
+        GIT_TAG         1.1.3
+        GIT_PROGRESS    TRUE
+        GIT_SHALLOW     TRUE
+    )
+
+    FetchContent_MakeAvailable(dragonbox)
+endif()
+
--- tools/cmake/fetchcontent.cmake
+++ tools/cmake/fetchcontent.cmake
@@ -1,6 +1,7 @@
 include(FetchContent)
 
 # NOTE: Cache dir not a standard FetchContent mechanism
+# DEBT: Not super Windows friendly, but won't cause any failures
 set(FETCHCONTENT_CACHE_DIR $ENV{HOME}/.local/cache/cmake/proj)
 if(EXISTS ${FETCHCONTENT_CACHE_DIR})
     set(FETCHCONTENT_BASE_DIR ${FETCHCONTENT_CACHE_DIR}/${PROJECT_NAME}-${PROJECT_VERSION})
