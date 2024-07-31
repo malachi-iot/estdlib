06470fd8309140c6bd46edebc148f693fad0bf58
Malachi Burke <malachi.burke@gmail.com>
2024-07-31 09:33:53 -0700

    fallthrough/nodiscard macros.  Just needs testing now
    https://github.com/malachi-iot/estdlib/issues/43

--- src/estd/internal/macros.h
+++ src/estd/internal/macros.h
@@ -53,3 +53,13 @@
 #define NOEXCEPT
 #endif
 
+// DEBT: Put these elsewhere
+// NOTE: Not using feature test has_macro since I've seen that glitch
+// out in llvm IIRC
+#if __cplusplus >= 201703L
+#define ESTD_CPP_ATTR_FALLTHROUGH   [[fallthrough]]
+#define ESTD_CPP_ATTR_NODISCARD     [[nodiscard]]
+#else
+#define ESTD_CPP_ATTR_FALLTHROUGH
+#define ESTD_CPP_ATTR_NODISCARD
+#endif
