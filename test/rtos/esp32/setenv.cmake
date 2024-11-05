b2efc08f3349efbf56a2aba89b20168f38ab44e0
Malachi Burke <malachi.burke@gmail.com>
2024-11-05 08:00:18 -0800

    refactor: phasing out "estdlib" in favor of "estd"

--- test/rtos/esp32/setenv.cmake
+++ test/rtos/esp32/setenv.cmake
@@ -1,13 +1,13 @@
-set(ESTDLIB_DIR ../../../..)
+set(ROOT_DIR ../../../..)
 #set(ESTDLIB_DIR "dummy")
 
 # DEBT: 23JUL23 Fixed this line but haven't tested yet
 get_filename_component(ESTDLIB_DIR
-    "${CMAKE_CURRENT_LIST_DIR}/${ESTDLIB_DIR}"
+    "${CMAKE_CURRENT_LIST_DIR}/${ROOT_DIR}"
     ABSOLUTE)
 
-#message("ESTDLIB building: ${ESTDLIB_DIR}")
+#message("ESTDLIB building: ${ROOT_DIR}")
 
-include(${ESTDLIB_DIR}/tools/esp-idf/project.cmake)
+include(${ROOT_DIR}/tools/esp-idf/project.cmake)
 include($ENV{IDF_PATH}/tools/cmake/project.cmake)
 
