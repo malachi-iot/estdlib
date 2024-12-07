be5677d7e1278598334e3663c0532d18ad1ed859
Malachi Burke <malachi.burke@gmail.com>
2024-12-06 23:25:33 -0800

    build: adding CTest stage to CI, prepping CPM use

--- tools/cmake/setvars.cmake
+++ tools/cmake/setvars.cmake
@@ -1,6 +1,9 @@
+include_guard()
+
 get_filename_component(TOOLS_DIR ${CMAKE_CURRENT_LIST_DIR} ABSOLUTE)
 get_filename_component(ROOT_DIR ${TOOLS_DIR}/../.. ABSOLUTE)
 set(ESTD_DIR ${ROOT_DIR}/src)
 
+include(${TOOLS_DIR}/CPM.cmake)
 include(${TOOLS_DIR}/fetchcontent.cmake)
 include(${TOOLS_DIR}/options.cmake)
