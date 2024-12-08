96c3e64fc2ffc6cbe2d6c583e10925982458563f
Malachi Burke <malachi.burke@gmail.com>
2024-12-08 09:10:16 -0800

    feat: adding bits of logging emulation
    https://github.com/malachi-iot/estdlib/issues/58

---
+++ src/estd/port/esp-idf/fake/esp_err.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef int esp_err_t;
+
+#define ESP_OK 0
+
+#ifdef __cplusplus
+}
+#endif
--- src/estd/port/esp-idf/fake/esp_log.h
+++ src/estd/port/esp-idf/fake/esp_log.h
@@ -2,11 +2,36 @@
 // DEBT: Would be better to use a wholistic logging system which included ESP target
 #pragma once
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 #include <stdio.h>
 
+//#define LOG_LOCAL_LEVEL
+
+//#define ESP_LOG_LEVEL
+
+enum esp_log_level_t
+{
+    ESP_LOG_NONE,
+    ESP_LOG_ERROR,
+    ESP_LOG_WARN,
+    ESP_LOG_INFO,
+    ESP_LOG_DEBUG,
+    ESP_LOG_VERBOSE
+};
+
+#define ESP_LOGE(tag, fmt...)     { printf("E %s: ", tag); printf(fmt); puts(""); }
 #define ESP_LOGW(tag, fmt...)     { printf("W %s: ", tag); printf(fmt); puts(""); }
 #define ESP_LOGI(tag, fmt...)     { printf("I %s: ", tag); printf(fmt); puts(""); }
 #define ESP_LOGD(tag, fmt...)     { printf("D %s: ", tag); printf(fmt); puts(""); }
 #define ESP_LOGV(tag, fmt...)     { printf("V %s: ", tag); printf(fmt); puts(""); }
 
 #define ESP_LOG_BUFFER_HEX(tag, buffer, len)
+
+void esp_log_write(esp_log_level_t level, const char *tag, const char *format, ...);
+
+#ifdef __cplusplus
+}
+#endif
