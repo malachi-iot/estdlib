531328c77858f3014a2fe79d845670ce6cc337c0
Malachi Burke <malachi.burke@gmail.com>
2025-01-03 13:19:40 -0800

    test(byteswap): successful ESP32C6 testing
    https://github.com/malachi-iot/estdlib/issues/77

--- test/unity/bit.cpp
+++ test/unity/bit.cpp
@@ -8,9 +8,9 @@ TEST_CASE("bit/endian tests", "[bit]")
 void test_bit()
 #endif
 {
-    auto vu16 = estd::byteswap<uint16_t>(0x1234);
-    auto vi32 = estd::byteswap<int32_t>(-500);
+    constexpr auto vu16 = estd::byteswap<uint16_t>(0x1234);
+    constexpr auto vi32 = estd::byteswap<int32_t>(-500);
 
-    TEST_ASSERT_EQUAL(0x3421, vu16);
+    TEST_ASSERT_EQUAL(0x3412, vu16);
     TEST_ASSERT_EQUAL(-500, estd::byteswap(vi32));
 }
