a4fc93a7b58584d852a703f004d48515a750df46
Malachi Burke <malachi.burke@gmail.com>
2025-02-22 20:56:42 -0800

    test: adding rtc clock, simple test works OK

--- test/rtos/esp32/chrono/main/main.cpp
+++ test/rtos/esp32/chrono/main/main.cpp
@@ -12,11 +12,14 @@ extern "C" void test_task(void* pv)
 
         auto now = chrono::esp_clock::now();
         auto now_freertos = chrono::freertos_clock::now();
+        auto now_rtc = chrono::esp_rtc_clock::now();
         auto now_ms = chrono::milliseconds(now.time_since_epoch());
         auto now_freertos_ms = chrono::milliseconds(now_freertos.time_since_epoch());
+        auto now_rtc_ms = chrono::milliseconds(now_rtc.time_since_epoch());
 
-        printf("Time passed: (native)=%lld / (rtos)=%lld\n",
+        printf("Time passed: (native)=%lld / (rtos)=%lld / (rtc)=%llu\n",
             now_ms.count(),
-            now_freertos_ms.count());
+            now_freertos_ms.count(),
+            now_rtc_ms.count());
     }
 }
