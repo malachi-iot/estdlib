36b4bafb2357e666fae9f429510f583080cca9b6
Malachi Burke <malachi.burke@gmail.com>
2025-02-22 20:52:21 -0800

    feat(rtc): adding esp_rtc_clock
    Untested https://github.com/malachi-iot/estdlib/issues/54

--- src/estd/port/esp-idf/chrono.h
+++ src/estd/port/esp-idf/chrono.h
@@ -1,25 +1,47 @@
 #pragma once
 
-#include "esp_timer.h"
+#include <esp_timer.h>
+#include <rtc.h>
 
 #include "../../internal/chrono/duration.h"
 #include "../../internal/chrono/time_point.h"
 
 namespace estd { namespace chrono {
 
+namespace internal {
+
+template <typename Rep>
 struct esp_clock
 {
-    typedef estd::chrono::internal::micro_rep rep;
-    typedef micro period;
-    typedef internal::estd_chrono::duration<rep, period> duration;
-    typedef internal::estd_chrono::time_point<esp_clock> time_point;
+    using rep = Rep;
+    using period = micro;
+    using duration = internal::estd_chrono::duration<rep, period>;
 
-    static CONSTEXPR bool is_steady = true;
+    static constexpr bool is_steady = true;
+};
+
+}
+
+struct esp_clock : internal::esp_clock<int64_t>
+{
+    using time_point = internal::estd_chrono::time_point<esp_clock>;
 
     static time_point now()
     {
         return time_point(duration(esp_timer_get_time()));
     }
 };
+
+
+struct esp_rtc_clock : internal::esp_clock<uint64_t>
+{
+    using time_point = internal::estd_chrono::time_point<esp_rtc_clock>;
+
+    static time_point now()
+    {
+        return time_point(duration(esp_rtc_get_time_us()));
+    }
+};
+
 
 }}
