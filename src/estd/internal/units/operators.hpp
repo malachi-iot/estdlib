975dbca502ebab6fc0f7a56efd4fb6b480d0272c
Malachi Burke <malachi.burke@gmail.com>
2025-12-29 08:56:51 -0800

    refactor(units): bringing comparisons into estd::units::detail

--- src/estd/internal/units/operators.hpp
+++ src/estd/internal/units/operators.hpp
@@ -5,118 +5,76 @@
 #include "fwd.h"
 #include "common_type.h"
 
-namespace estd { namespace internal { namespace units {
+namespace estd { namespace units { inline namespace v1 { namespace detail {
 
-// DEBT: Consolidate with all the clever chrono "common_type" versions - for now
-// implementing only the most basic to avoid too much code duplication
+using internal::units::ct_helper;
 
 template <class Traits>
-constexpr v2::unit_base<Traits> operator +(
-    const v2::unit_base<Traits>& lhs,
-    const v2::unit_base<Traits>& rhs)
+constexpr unit<Traits> operator +(
+    const unit<Traits>& lhs,
+    const unit<Traits>& rhs)
 {
-    return v2::unit_base<Traits>{ (typename Traits::rep) (lhs.root_count() + rhs.root_count()) };
+    return unit<Traits>{ (typename Traits::rep) (lhs.root_count() + rhs.root_count()) };
 }
+
 
 template <class Traits1, class Traits2>
 constexpr auto operator +(
-    const v2::unit_base<Traits1>& lhs,
-    const v2::unit_base<Traits2>& rhs) -> decltype(ct_helper(lhs, rhs))
+    const unit<Traits1>& lhs,
+    const unit<Traits2>& rhs) -> decltype(ct_helper(lhs, rhs))
 {
     using CT = decltype(ct_helper(lhs, rhs));
 
     return CT(lhs) + CT(rhs);
 }
 
+
 template <class Traits>
-constexpr v2::unit_base<Traits> operator -(
-    const v2::unit_base<Traits>& lhs,
-    const v2::unit_base<Traits>& rhs)
+constexpr unit<Traits> operator -(
+    const unit<Traits>& lhs,
+    const unit<Traits>& rhs)
 {
-    return v2::unit_base<Traits>{ typename Traits::rep(lhs.root_count() - rhs.root_count()) };
+    return unit<Traits>{ typename Traits::rep(lhs.root_count() - rhs.root_count()) };
 }
 
 
 template <class Traits1, class Traits2>
 constexpr auto operator -(
-    const v2::unit_base<Traits1>& lhs,
-    const v2::unit_base<Traits2>& rhs) -> decltype(ct_helper(lhs, rhs))
+    const unit<Traits1>& lhs,
+    const unit<Traits2>& rhs) -> decltype(ct_helper(lhs, rhs))
 {
     using CT = decltype(lhs + rhs);
 
     return CT(lhs) - CT(rhs);
 }
 
+// Comparisons
+
+#define ESTD_UNITS_COMP(op) \
+template <class Traits1, class Traits2> \
+constexpr bool operator op(const unit<Traits1>& lhs, const unit<Traits2>& rhs) \
+{   \
+    using CT = decltype(lhs + rhs); \
+    return CT(lhs).count() op CT(rhs).count(); \
+}
+
+ESTD_UNITS_COMP(>)
+ESTD_UNITS_COMP(<)
+ESTD_UNITS_COMP(>=)
+ESTD_UNITS_COMP(<=)
+ESTD_UNITS_COMP(==)
+ESTD_UNITS_COMP(!=)
+
+
+}}}}
+
+namespace estd { namespace internal { namespace units {
+
+// DEBT: Consolidate with all the clever chrono "common_type" versions - for now
+// implementing only the most basic to avoid too much code duplication
 
 // TODO: Add operator* and operator-.  They work well enough BUT we may not want to return
 // the same CT in those cases
-
-
-template <class Traits1, class Traits2>
-constexpr bool operator>(
-    const v2::unit_base<Traits1>& lhs,
-    const v2::unit_base<Traits2>& rhs)
-{
-    using CT = decltype(lhs + rhs);
-
-    return CT(lhs).count() > CT(rhs).count();
-}
-
-
-template <class Traits1, class Traits2>
-constexpr bool operator<(
-    const v2::unit_base<Traits1>& lhs,
-    const v2::unit_base<Traits2>& rhs)
-{
-    using CT = decltype(lhs + rhs);
-
-    return CT(lhs).count() < CT(rhs).count();
-}
-
-
-template <class Traits1, class Traits2>
-constexpr bool operator>=(
-    const v2::unit_base<Traits1>& lhs,
-    const v2::unit_base<Traits2>& rhs)
-{
-    using CT = decltype(lhs + rhs);
-
-    return CT(lhs).count() >= CT(rhs).count();
-}
-
-
-template <class Traits1, class Traits2>
-constexpr bool operator<=(
-    const v2::unit_base<Traits1>& lhs,
-    const v2::unit_base<Traits2>& rhs)
-{
-    using CT = decltype(lhs + rhs);
-
-    return CT(lhs).count() <= CT(rhs).count();
-}
-
-
-
-template <class Traits1, class Traits2>
-constexpr bool operator==(
-    const v2::unit_base<Traits1>& lhs,
-    const v2::unit_base<Traits2>& rhs)
-{
-    using CT = decltype(lhs + rhs);
-
-    return CT(lhs).count() == CT(rhs).count();
-}
-
-
-template <class Traits1, class Traits2>
-constexpr bool operator!=(
-    const v2::unit_base<Traits1>& lhs,
-    const v2::unit_base<Traits2>& rhs)
-{
-    using CT = decltype(lhs + rhs);
-
-    return CT(lhs).count() != CT(rhs).count();
-}
 
 // ratio<1> specializations
 
