1e04789e5d2d3adabd81bfad4c5098f40e374628
Malachi Burke <malachi.burke@gmail.com>
2026-01-15 11:53:36 -0800

    feat: bits of progress, does render now if you force it to load

--- tools/gdb/pretty.py
+++ tools/gdb/pretty.py
@@ -9,17 +9,29 @@ class UnitPrinter:
 
     # Save a copy of the Location.
     def __init__(self, val):
+        #print('GOT HERE 1')
         self.__val = val
 
     def to_string(self):
-        return "XYZ"
+        return self.__val['rep_']
+
+    def children(self):
+        rep = int(self.to_string())
+        print(rep)
+        return [
+            ("count()", rep)
+        ]
+
+    def display_hint(self):
+        print('GOT HERE 4')
+        return 'string'
 
 def UnitPrinter_func(val):
     t = str(val.type)
-    if t.startswith('estd::units::detail::v1::unit'):
+    if t.startswith('estd::units::v1::detail::unit<'):
         return UnitPrinter(val)
     return None
 
 print('Registering estd types')
-# Add the function to gdb's list of PrettyPrinter functions.
+
 gdb.pretty_printers.append(UnitPrinter_func)
