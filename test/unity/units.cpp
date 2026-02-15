d7dcf9b3daa888fc3baa8c93a472e1691c184a54
Malachi Burke <malachi.burke@gmail.com>
2026-02-15 10:52:53 -0800

    test: identifying now-too-permissive unit construction

--- test/unity/units.cpp
+++ test/unity/units.cpp
@@ -10,15 +10,22 @@ TEST_CASE("units (percent, bytes)", "[units]")
 void test_units()
 #endif
 {
-    units::percent<int16_t> v1(5);
-    units::percent<int> v2(5);
+    {
+        units::percent<int16_t> v1(5);
+        units::percent<int> v2(5);
 
-    // A bunch of misc things to poke and prod common_type and friends
-    v1 = v1 * 5;
-    v2 = v1 * 5;
+        // A bunch of misc things to poke and prod common_type and friends
+        v1 = v1 * 5;
+        v2 = v1 * 5;
 
-    TEST_ASSERT_EQUAL(125, v2.count());
+        TEST_ASSERT_EQUAL(125, v2.count());
 
-    v2 = v2 * 5;
-    v2 = v2 + v1;
+        v2 = v2 * 5;
+        v2 = v2 + v1;
+    }
+    {
+        // FIX: Now we are too permissive, this shouldn't compile
+        units::percent<uint32_t> v1(5);
+        units::percent<int16_t> v2(v1);
+    }
 }
