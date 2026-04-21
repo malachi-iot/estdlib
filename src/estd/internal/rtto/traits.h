3d3959b982b6c07c22010099ded7e88227a96732
Malachi Burke <malachi.burke@gmail.com>
2026-04-21 08:50:45 -0700

    refactor: dogfooding our own type_traits

--- src/estd/internal/rtto/traits.h
+++ src/estd/internal/rtto/traits.h
@@ -1,5 +1,9 @@
 #pragma once
 
+#include "fwd.h"
+
+#include "../type_traits.h"
+
 namespace estd { namespace internal {
 
 // DEBT: CREATE mode is experimental and is_default_constructible falls apart for forwarding constructors,
@@ -11,9 +15,10 @@ struct rtto_traits
     using value_type = T;
 
     // DEBT: Switch these to estd variety for AVR compability
-    using is_copy_constructible = std::is_copy_constructible<value_type>;
-    using is_move_constructible = std::is_move_constructible<value_type>;
-    using is_trivially_constructible = std::is_trivially_constructible<value_type>;
+    using is_copy_constructible = estd::is_copy_constructible<value_type>;
+    using is_move_constructible = estd::is_move_constructible<value_type>;
+    using is_trivially_constructible = estd::is_trivially_constructible<value_type>;
+    using is_trivially_moveable = estd::is_trivially_move_constructible<value_type>;
     //using is_constructible = std::is_default_constructible<value_type>;
     using is_constructible = IsDefaultConstructible;
 #if __cpp_lib_is_swappable
