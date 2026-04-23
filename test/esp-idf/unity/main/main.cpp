a1bd0747579c59ea5ff39eba74291c3786839514
Malachi Burke <malachi.burke@gmail.com>
2026-04-23 10:33:36 -0700

    build: experimenting with IDF 6.0 qemu no reboot option

--- test/esp-idf/unity/main/main.cpp
+++ test/esp-idf/unity/main/main.cpp
@@ -12,8 +12,12 @@ extern "C" void app_main(void)
     unity_run_all_tests();
     UNITY_END();
 
+#if CONFIG_ESTD_CI_MODE
+    exit(0);
+#else
     /* This function will not return, and will be busy waiting for UART input.
      * Make sure that task watchdog is disabled if you use this function.
      */
     unity_run_menu();
+#endif
 }
