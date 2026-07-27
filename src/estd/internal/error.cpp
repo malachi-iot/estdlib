5c9b8aa0559fafb4bc3f3308be73e73279808a58
Malachi Burke <malachi.burke@gmail.com>
2026-07-27 11:31:54 -0700

    feat: error_traits now tightly coupled with category

--- src/estd/internal/error.cpp
+++ src/estd/internal/error.cpp
@@ -6,15 +6,21 @@ namespace estd { namespace internal {
 
 template <>
 template <>
-const char* error_traits<errc>::to_string<system_category_tag>(errc err)
+const char* error_traits<errc, generic_category_tag>::to_string<classic_locale_type>(errc err)
 {
     switch(err)
     {
         CASE(address_family_not_supported)
+        CASE(host_unreachable)
         CASE(invalid_argument)
+        CASE(io_error)
+        CASE(no_space_on_device)
+        CASE(no_message)
         CASE(not_enough_memory)
         CASE(resource_unavailable_try_again)
         CASE(result_out_of_range)
+        CASE(timed_out)
+        CASE(value_too_large)
         default: return "N/A";
     }
 }
--- src/estd/internal/error_code.h
+++ src/estd/internal/error_code.h
@@ -2,6 +2,7 @@
 
 #include "errc.h"
 #include "../string_view.h"
+#include "../locale.h"
 
 
 namespace estd {
@@ -22,41 +23,39 @@ namespace internal {
 struct system_category_tag {};  // platform-specific, more or less
 struct generic_category_tag {}; // portable, more or less
 
-template <class ErrorCodeEnum>
+template <class ErrorCodeEnum, class Tag>
 struct error_traits
 {
     using value_type = ErrorCodeEnum;
+    using category = Tag;
 
-    template <class CategoryTag>
+    template <class Locale = classic_locale_type>
     static const char* to_string(value_type);
 
-    template <class CategoryTag>
+    template <class ErrorTraits2>
     static int map_to(value_type v);
 };
 
+// On Linux, this usually maps POSIX error codes.  Not fleshed out
+using system_error_traits = error_traits<int, system_category_tag>;
+
+// generic_category is more or less 1:1 with errc
+using generic_error_traits = error_traits<errc, generic_category_tag>;
+
 template <>
 template <>
-constexpr int error_traits<errc>::map_to<system_category_tag>(value_type v)
+constexpr int error_traits<errc, generic_category_tag>::map_to<generic_error_traits>(value_type v)
 {
     return int(v);
 }
 
-template <>
-template <>
-constexpr int error_traits<errc>::map_to<generic_category_tag>(value_type v)
-{
-    return int(v);
-}
-
-using system_error_traits = error_traits<errc>;
-
-template <class ErrorTraits, class CategoryTag = generic_category_tag>
+template <class ErrorTraits>
 class error_condition;
 
-template <class ErrorTraits, class CategoryTag = system_category_tag>
+template <class ErrorTraits>
 class error_code;
 
-template <class ErrorTraits, class CategoryTag>
+template <class ErrorTraits>
 class error_category
 {
 public:
@@ -65,18 +64,18 @@ public:
 
     static constexpr string_view message(int condition)
     {
-        return traits::template to_string<CategoryTag>(static_cast<value_type>(condition));
+        return traits::to_string(static_cast<value_type>(condition));
     }
 
     // 27JUL26 MB FIX: This still just doesn't line up with std flavor's philosophy
-    template <class CategoryTag2 = generic_category_tag>
-    static constexpr error_condition<ErrorTraits, CategoryTag2> default_error_condition(int val)
+    template <class ErrorTraits2>
+    static constexpr error_condition<ErrorTraits2> default_error_condition(int val)
     {
-        return { int(traits::template map_to<CategoryTag2>(static_cast<value_type>(val))) };
+        return { int(traits::template map_to<ErrorTraits2>(static_cast<value_type>(val))) };
     }
 };
 
-template <class ErrorTraits, class CategoryTag>
+template <class ErrorTraits>
 class error_base
 {
 protected:
@@ -85,7 +84,7 @@ protected:
     constexpr error_base(int ec) : ec_{ec}  {}
 
 public:
-    using category_type = error_category<ErrorTraits, CategoryTag>;
+    using category_type = error_category<ErrorTraits>;
 
     static constexpr category_type category() { return {}; }
 
@@ -98,20 +97,20 @@ public:
 };
 
 // "holds a platform-independent value identifying an error condition"
-template <class ErrorTraits, class CategoryTag>
-class error_condition : public error_base<ErrorTraits, CategoryTag>
+template <class ErrorTraits>
+class error_condition : public error_base<ErrorTraits>
 {
-    using base_type = error_base<ErrorTraits, CategoryTag>;
+    using base_type = error_base<ErrorTraits>;
 
 public:
     constexpr error_condition(int ec) : base_type{ec}   {}
 };
 
 // "represents a platform-dependent error code value"
-template <class ErrorTraits, class CategoryTag>
-class error_code : public error_base<ErrorTraits, CategoryTag>
+template <class ErrorTraits>
+class error_code : public error_base<ErrorTraits>
 {
-    using base_type = error_base<ErrorTraits, CategoryTag>;
+    using base_type = error_base<ErrorTraits>;
     using typename base_type::category_type;
 
 public:
@@ -120,21 +119,21 @@ public:
 
     constexpr error_code(value_type ec) : base_type{static_cast<int>(ec)}   {}
 
-    template <class CategoryTag2 = generic_category_tag>
-    constexpr error_condition<ErrorTraits, CategoryTag2> default_error_condition() const
+    template <class ErrorTraits2 = generic_error_traits>
+    constexpr error_condition<ErrorTraits2> default_error_condition() const
     {
-        return base_type::category().template default_error_condition<CategoryTag2>(base_type::ec_);
+        return base_type::category().template default_error_condition<ErrorTraits2>(base_type::ec_);
     }
 };
 
 }
 
-constexpr internal::error_code<internal::system_error_traits> make_error_code(errc e) noexcept
+constexpr internal::error_code<internal::generic_error_traits> make_error_code(errc e) noexcept
 {
     return { e };
 }
 
-constexpr internal::error_condition<internal::system_error_traits> make_error_condition(errc e) noexcept
+constexpr internal::error_condition<internal::generic_error_traits> make_error_condition(errc e) noexcept
 {
     return { int(e) };
 }
